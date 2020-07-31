d02e1e168e6e1290970257e181d27fc9db0b4c24
ulaulaman <gianluigi.ulaula@gmail.com>
2020-07-31 22:04:21 +0200

    return to old version

--- citations-tools.php
+++ citations-tools.php
@@ -14,7 +14,7 @@ License: GPLv2 or later
 #plugin dir
 define( 'CT__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 #define( 'CT_PLUGIN_FILE', __FILE__ );
-require_once( CT_PLUGIN_FILE . 'button.php' );
+require_once( CT__PLUGIN_DIR . 'button.php' );
 
 # doi
 add_shortcode('ctdoi', 'ctdoi');
