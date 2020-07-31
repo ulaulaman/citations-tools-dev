ef8de50261d8e93c637a942107b9c2c91a2ecc9d
ulaulaman <gianluigi.ulaula@gmail.com>
2020-07-31 18:50:10 +0200

    button php

---
+++ button.php
@@ -0,0 +1,21 @@
+<?php
+
+function tiny_mce_add_buttons( $plugins ) {
+    $plugins['mytinymceplugin'] = get_template_directory_uri() . '/button.js';
+    return $plugins;
+  }
+  
+  function tiny_mce_register_buttons( $buttons ) {
+    $newBtns = array(
+      'citationsbtn'
+    );
+    $buttons = array_merge( $buttons, $newBtns );
+    return $buttons;
+  }
+
+add_action( 'init', 'tiny_mce_new_buttons' );
+
+function tiny_mce_new_buttons() {
+  add_filter( 'mce_external_plugins', 'tiny_mce_add_buttons' );
+  add_filter( 'mce_buttons', 'tiny_mce_register_buttons' );
+}
