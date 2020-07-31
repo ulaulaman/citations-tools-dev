4a6c389ae3754a577aa62162ab88b2e13ae222bd
ulaulaman <gianluigi.ulaula@gmail.com>
2020-07-31 21:49:28 +0200

    rewrite

--- button.js
+++ button.js
@@ -1,175 +1,65 @@
-(function(){
-    tinymce.create('tinymce.plugins.CitationsTools', {
-    
-        init: function(_ed, _url){
-  
-        },
-        getInfo: function() {
-          return {
-            longname: 'Citations Tools Button',
-            author: 'Plugin Author',
-            authorurl: 'https://www.axosoft.com',
-            version: "1.0"
-          };
-        },
+jQuery(function ($) {
 
-        ed.addButton('citationsbtn', {
-            title: 'Insert Paper Citation',
-            cmd: 'CitationsBtnCmd',
-            image: `${url}/img/ct-icon.png`
-          }),
+    tinymce.create('tinymce.plugins.paperdata_plugin', {
 
-        ed.addCommand('CitationsBtnCmd', function(){
-            var selectedText = ed.selection.getContent({format: 'html'});
-            var win = ed.windowManager.open({
-              title: 'Properties',
-              body: [
-                {
-                  type: 'textbox',
-                  name: 'auth',
-                  label: 'Author/s',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'year',
-                  label: 'Year',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'title',
-                  label: 'Title',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'journal',
-                  label: 'Journal',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'vol',
-                  label: 'vol',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'code',
-                  label: 'Doi',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'issue',
-                  label: 'issue',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'arxiv',
-                  label: 'arXive code',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'pages',
-                  label: 'pages',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'pdfurl',
-                  label: 'Pdf url',
-                  minWidth: 500,
-                  value: ''
-                },
-                {
-                    type: 'textbox',
-                    name: 'archiveurl',
-                    label: 'archive.org url',
-                    minWidth: 500,
-                    value: ''
-                  }
-              ],
-              buttons: [
-                {
-                  text: "Ok",
-                  subtype: "primary",
-                  onclick: function() {
-                    win.submit();
-                  }
-                },
-                {
-                  text: "Skip",
-                  onclick: function() {
-                    win.close();
-                    var returnText = '[ blockquote]' + selectedText + '[/blockquote ] ';
-                    ed.execCommand('mceInsertContent', 0, returnText);
-                  }
-                },
-                {
-                  text: "Cancel",
-                  onclick: function() {
-                    win.close();
-                  }
-                }
-              ],
-              onsubmit: function(e){
-                var params = [];
-                if( e.data.auth.length > 0 ) {
-                  params.push('auth="' + e.data.author + '"');
-                }
-                if( e.data.year.length > 0 ) {
-                    params.push('year="' + e.data.author + '"');
-                }
-                if( e.data.title.length > 0 ) {
-                  params.push('title="' + e.data.author + '"');
-                }
-                if( e.data.journal.length > 0 ) {
-                    params.push('journal="' + e.data.author + '"');
-                }
-                if( e.data.vol.length > 0 ) {
-                    params.push('vol="' + e.data.author + '"');
-                }
-                if( e.data.code.length > 0 ) {
-                    params.push('code="' + e.data.author + '"');
-                }
-                if( e.data.issue.length > 0 ) {
-                  params.push('issue="' + e.data.cite + '"');
-                }
-                if( e.data.arxiv.length > 0 ) {
-                  params.push('arxiv="' + e.data.link + '"');
-                }
-                if( e.data.pages.length > 0 ) {
-                    params.push('pages="' + e.data.author + '"');
-                }
-                if( e.data.pdfurl.length > 0 ) {
-                    params.push('pdfurl="' + e.data.author + '"');
-                }
-                if( e.data.archiveurl.length > 0 ) {
-                    params.push('archiveurl="' + e.data.author + '"');
-                }
-                if( params.length > 0 ) {
-                  paramsString = ' ' + params.join(' ');
-                }
-                var returnText = '[paperdata' + paramsString + ']';
-                ed.execCommand('mceInsertContent', 0, returnText);
-              }
+        init: function (ed, url) {
+
+            ed.addButton('paperdata_button', {
+                title: 'Add Paper Data',
+                text: 'Paper Data',
+                cmd: 'ct_insert_shortcode'
             });
-          }),
 
-    });
+            ed.addCommand('ct_insert_shortcode', fuction () {
 
-    tinymce.PluginManager.add( 'mytinymceplugin', tinymce.plugins.CitationsTools );
-  })();
+                var selected = tinyMCE.activeEditor.selection.getContent();
+                var content;
+
+                ed.windowManager.open({
+                    title: 'Insert paper data',
+                    body: [{
+                        type: 'textbox',
+                        name: 'auth',
+                        label: 'Author/s'
+                    },
+                    {
+                        type: 'textbox',
+                        name: 'year',
+                        label: 'Year'
+                    },
+                    {
+                        type: 'textbox',
+                        name: 'title',
+                        label: 'Title'
+                    },
+                    {
+                        type: 'textbox',
+                        name: 'journal',
+                        label: 'Journal'
+                    }
+                    ],
+                    onsubmit: function (e) {
+                        var shortcode = '[paperdata "' + e.data.auth + '" ';
+                            if (e.data.year !== '') {
+                                shortcode += ' height="' + e.data.year + '" ';
+                            }
+                            if (e.data.title !== '') {
+                                shortcode += ' width="' + e.data.title + '" ';
+                            }
+							if (e.data.journal !== '') {
+                                shortcode += ' width="' + e.data.journal + '" ';
+                            }
+                            shortcode += ' ]';
+
+                            ed.insertContent(shortcode);
+                    }
+                });
+            });
+
+        }
+    }
+    )
+
+    tinymce.PluginManager.add('paperdata_button', tinymce.plugins.paperdata_plugin);
+
+});
